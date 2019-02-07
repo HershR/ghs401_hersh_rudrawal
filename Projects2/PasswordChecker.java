20a2ece7a9c7029650ded173d04e39ed7111ff80
Hersh Rudrawal <hersh.rud@lvjusd.k12.ca.us>
2019-02-07 13:31:49 -0800

    2/7/2019

--- Projects2/PasswordChecker.java
+++ Projects2/PasswordChecker.java
@@ -10,15 +10,25 @@ public class PasswordChecker
     public static void main()
     {
         String thing = JOptionPane.showInputDialog("Which school do you go to?");
-        if(thing.toLowerCase().equals(school.toLowerCase()))
-        {
-            String thing2 = JOptionPane.showInputDialog("Cool. What's your school's mascot?");
-            if(thing2.toLowerCase().compareTo(mascot.toLowerCase())==0)
-            {
+        String thing2 = JOptionPane.showInputDialog("Cool. What's your school's mascot?");
+        if(thing.toLowerCase().equals(school.toLowerCase())){
+            if(thing2.toLowerCase().compareTo(mascot.toLowerCase())==0){
                 String thing3 = JOptionPane.showInputDialog("AwEsOmE. Lastly, what state do you live in?");
                 if(thing3.toLowerCase().compareTo(state.toLowerCase())==0){
                     System.out.println("Welcome");
-               
+                    while(!length){
+                        String pass =JOptionPane.showInputDialog("Enter Password:");
+                        if(pass.length()>5){
+                            if(pass.contains("+")|| pass.contains("-") || pass.contains("*")|| pass.contains("@")){
+                                System.out.println("You have a good password");
+                                length=true;      
+                            }else{
+                                System.out.println("Create a password with symbols");
+                            }
+                        }else{
+                            System.out.println("Create a password with six letters/numbers or more!");
+                        }
+                    }
                 }else if(thing3.compareTo(state.toLowerCase())<0){
                     System.out.println("Too early in the alphabet");
                 }else if(thing3.compareTo(state.toLowerCase())>0){
@@ -26,32 +36,18 @@ public class PasswordChecker
                 }
             }else{
                 System.out.println("Wrong mascot idiot...");
-            }          
-        }
-        else{
+            }     
+        }else{
             System.out.println("Wrong school idiot...");
-            System.out.println(thing);
         }
         
-        while(!length){
-            String pass =JOptionPane.showInputDialog("Enter Password:");
-            if(pass.length()>5){
-                if(pass.contains("+")|| pass.contains("-") || pass.contains("*")|| pass.contains("@")){
-                      System.out.println("You have a good password");
-                      length=true;      
-                }else{
-                    System.out.println("Create a password with symbols");
-                }
-            }else{
-                    System.out.println("Create a password with six letters or more!");
-                }
-        }
+        
         
         
         //String pass =JOptionPane.showInputDialog("Enter password:");
         //while(pass.length()<6 || pass.contains("+")|| pass.contains("-") || pass.contains("*")|| pass.contains("@")){
-        //    pass =JOptionPane.showInputDialog("Enter stronger password:");
-        //    
+        //  pass =JOptionPane.showInputDialog("Enter stronger password:");
+        //
         //}
         //System.out.println("ok");
     }
--- Projects2/watson.java
+++ Projects2/watson.java
@@ -14,18 +14,22 @@ public class watson
        for(int i =0;i<elementList.size();i++){
            System.out.println(elementList.get(i)); 
        }
-       System.out.println("The elements containg 'on' are: ");
+       System.out.println("\nThe elements containg 'on' are: ");
        for(int i =0;i<elementList.size();i++){
            if(elementList.get(i).contains("on")){
                System.out.println(elementList.get(i));
            }
        }
-       System.out.println("Remove all elements alphabetically after 'd': ");
+       System.out.println("\nRemove all elements alphabetically after 'd': ");
        for(int i =0;i<elementList.size();i++){
-           int result=0;
-           if(result==0){
-               System.out.println(elementList.get(i));
-           }
+           int result=elementList.get(i).compareTo("d");
+           if(result>0){
+               elementList.remove(i);
+               i=i-1;
+           }           
        }
+       for(int j =0;j<elementList.size();j++){
+               System.out.println("Still remaining: "+elementList.get(j)); 
+           }
    }
 }
